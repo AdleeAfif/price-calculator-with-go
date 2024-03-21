97e5bcdc2e9fd4c126d325660683d5bc65510a30
AdleeAfif <kamitokazuya@gmail.com>
2024-03-21 13:18:43 +0800

    Added IOManager struct for loose coupling and improve error handling

--- prices/prices.go
+++ prices/prices.go
@@ -3,18 +3,22 @@ package prices
 import (
 	"fmt"
 	"project/price-calculator/conversion"
-	"project/price-calculator/filemanager"
+	"project/price-calculator/iomanager"
 )
 
 type TaxIncludedPriceJob struct {
-	IOManager        filemanager.FileManager
-	TaxRate          float64
-	InputPrices      []float64
-	TaxIncludedPrice map[string]string
+	IOManager        iomanager.IOManager `json:"-"` //exclude from json
+	TaxRate          float64             `json:"tax_rate"`
+	InputPrices      []float64           `json:"input_prices"`
+	TaxIncludedPrice map[string]string   `json:"tax_included_price"`
 }
 
-func (job *TaxIncludedPriceJob) Process() {
-	job.LoadDataFromFile()
+func (job *TaxIncludedPriceJob) Process() error {
+	err := job.LoadData()
+	if err != nil {
+		return err
+	}
+
 	pricesAfterTax := make(map[string]string)
 	for _, price := range job.InputPrices {
 		result := price * (1 + job.TaxRate)
@@ -23,32 +27,32 @@ func (job *TaxIncludedPriceJob) Process() {
 
 	job.TaxIncludedPrice = pricesAfterTax
 
-	job.IOManager.WriteJSON(job)
+	return job.IOManager.WriteJSON(job)
 }
 
-func New(fm filemanager.FileManager, taxRate float64) *TaxIncludedPriceJob {
+func New(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
-		IOManager:   fm,
+		IOManager:   iom,
 		TaxRate:     taxRate,
 		InputPrices: []float64{10, 20, 30},
 	}
 }
 
-func (job *TaxIncludedPriceJob) LoadDataFromFile() {
+func (job *TaxIncludedPriceJob) LoadData() error {
 
 	fileLine, err := job.IOManager.ReadLines()
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
 	data, err := conversion.StringToFloat(fileLine)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		return err
 	}
 
 	job.InputPrices = data
+
+	return nil
 }
