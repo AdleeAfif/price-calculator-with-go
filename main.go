40974de6e940be5a5dcea9a887a26b6926649194
AdleeAfif <kamitokazuya@gmail.com>
2024-03-21 09:48:27 +0800

    Straight forward tax calculation

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.1, 0.2, 0.3}
+
+	pricesAfterTax := make(map[float64][]float64)
+
+	for _, tax := range taxRates {
+		for _, price := range prices {
+			pricesAfterTax[tax] = append(pricesAfterTax[tax], calculatePriceAfterTax(price, tax))
+		}
+	}
+
+	fmt.Println(pricesAfterTax)
+}
+
+func calculatePriceAfterTax(price float64, taxRate float64) float64 {
+	return price * (1 + taxRate)
 }
